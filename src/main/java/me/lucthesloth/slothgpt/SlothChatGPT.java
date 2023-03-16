34e0099cfbe8ec249f6f476c8cd2f4d1da7b3ade
BuildTools <unconfigured@null.spigotmc.org>
2023-03-16 01:21:31 -0300

    Removing Client Arguments, No need after switching to string only.

--- src/main/java/me/lucthesloth/slothgpt/SlothChatGPT.java
+++ src/main/java/me/lucthesloth/slothgpt/SlothChatGPT.java
@@ -24,7 +24,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -34,8 +33,6 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-
-import dev.xpple.clientarguments.arguments.CEntityArgumentType;
 
 public class SlothChatGPT implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("sloth-chatgpt");
