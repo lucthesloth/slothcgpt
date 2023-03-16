e7360a4ffbf56a81f6330960614918671c0f59fb
BuildTools <unconfigured@null.spigotmc.org>
2023-03-15 21:06:41 -0300

    Bungee Fix

--- src/main/java/me/lucthesloth/slothgpt/PlayersSuggestionProvider.java
+++ src/main/java/me/lucthesloth/slothgpt/PlayersSuggestionProvider.java
@@ -9,15 +9,17 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
 
 public class PlayersSuggestionProvider implements SuggestionProvider<FabricClientCommandSource>{
 
     @Override
     public CompletableFuture<Suggestions> getSuggestions(CommandContext<FabricClientCommandSource> context,
             SuggestionsBuilder builder) throws CommandSyntaxException {
-        context.getSource().getWorld().getPlayers().forEach(player -> {
-            builder.suggest(player.getName().getString());
-        });
+                MinecraftClient.getInstance().getNetworkHandler().getPlayerList().stream().forEach(player -> {
+                    builder.suggest(player.getProfile().getName());
+                });
+
         return builder.buildFuture();
     }
     
